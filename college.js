746bc404dffc6f75deb430ed7af8438ae82ee159
C. Josette Looft <41592275+cjlooft@users.noreply.github.com>
2026-02-10 12:50:32 -0600

    Update college.js

--- college.js
+++ college.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Check if time has run out
             if (timeLeft <= 0) {
-                endGame("Time's up! You did not complete the phrase in time.");
+                endGame("😱 Time's up! You did not complete the phrase in time.");
             }
         }, 1000); // Update every second
     }
